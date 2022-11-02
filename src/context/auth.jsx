cc761e008a93201ea3b7a45a76f2e48be62ccf06
sthaynny <igorsthaynny@gmail.com>
2022-11-02 18:19:41 -0300

    feat: criando navegação

--- src/context/auth.jsx
+++ src/context/auth.jsx
@@ -1,12 +1,21 @@
 import { createContext, useState } from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
 export const AuthContext = createContext();
 
 export const AuthProvider = ({children})=>{
+
+    const navigate = useNavigate();
     const [token, setToken]= useState(null)
     const login = (username, senha)=>{
+        
         console.log('login', {username, senha})
-        setToken( 'asd')
+        if(senha ==='senha'){
+            setToken('asd')
+            navigate('/')
+        }
+        
     }
     const logout = ()=>{
         console.log("logout")
