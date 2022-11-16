2c8129c537551a6ff56dc16f469a75c0369329c4
sthaynny <igorsthaynny@gmail.com>
2022-11-16 01:55:13 -0300

    feat: ajustanod chamada de emprestimos

--- src/pages/components/drawer/index.js
+++ src/pages/components/drawer/index.js
@@ -1,16 +1,33 @@
 import "./style.css";
 
+import React, { useContext } from 'react';
+
+import { AuthContext } from "../../../context/auth";
 import Drawer from 'react-modern-drawer';
 import { Link } from 'react-router-dom';
-import React from 'react';
 
 export const DrawerApp = (props) => {
     const isOpen = props.isOpen;
-    const toggleDrawer = props.toggleDrawer  
-    
+    const toggleDrawer = props.toggleDrawer
+
+    const { superUser } = useContext(AuthContext);
+
+    const ListDrawer = () => {
+        if (superUser) {
+            return <div>
+                <Link className='option' to='/cadastro-livro/'>Cadastro Livro</Link>
+                <Link className='option'>Emprestimos</Link>
+            </div>
+        } else {
+
+            return <div>
+                <Link className='option'>Meus Emprestimos</Link>
+            </div>
+        }
+    }
+
     return <Drawer open={isOpen} onClose={toggleDrawer} direction='left' style={{ backgroundColor: '#362E49', }}>
         <h2>Menu</h2>
-        <Link className='option' to='/cadastro-livro/'>Cadastro Livro</Link>
-        <Link className='option'>Emprestimos</Link>
+        <ListDrawer />
     </Drawer>
 }
